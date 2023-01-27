c59eb140c7787201f4b64e2c400c9449c233aeea
dias19 <dias.nurmukhambetov@gmail.com>
2023-01-27 18:44:54 +0600

    ..fixed imports

--- src/features/management/index.tsx
+++ src/features/management/index.tsx
@@ -1,4 +1,4 @@
 export * from './components/restaurants';
 export * from './components/user-profile';
 export * from './components/add-restaurants';
-export * from './components/restaurant-profile';
+export * from './components/restaurant-details/details';
